8b6ce6adda2ba69aa940579661bfc06c8ebc1bfa
NigarMajidzada <107548352+NigarMajidzada@users.noreply.github.com>
2025-11-05 21:59:11 +0400

    Update Tasks and solution.sql

--- Netflix/Tasks and solution.sql
+++ Netflix/Tasks and solution.sql
@@ -12,3 +12,8 @@ SELECT COUNT(TITLE) AS COUNT_OF_FILM FROM netflix_titles
 SELECT COUNT(DISTINCT TITLE)  AS COUNT_DISTINCT_FILM FROM netflix_titles
 
 
+
+--2 Kateqroiyalar uzrre rating siralamasi ve saylari 
+
+SELECT type, rating, COUNT(*) count_of_movie from netflix_titles
+GROUP BY type, rating
