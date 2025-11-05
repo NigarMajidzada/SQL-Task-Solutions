758e1fcfacac45696e0be55c82a82f3643c3db2f
NigarMajidzada <107548352+NigarMajidzada@users.noreply.github.com>
2025-11-05 21:42:40 +0400

    Add files via upload

---
+++ Netflix/Tasks and solution.sql
@@ -0,0 +1,14 @@
+use Netflix
+
+--SELECT COUNT(*) FROM netflix_titles
+
+--SELECT * FROM netflix_titles
+
+
+--1 TASK FILMLERIN SAYI VE UNIK FILMLERIN SAYI
+
+SELECT COUNT(TITLE) AS COUNT_OF_FILM FROM netflix_titles
+
+SELECT COUNT(DISTINCT TITLE)  AS COUNT_DISTINCT_FILM FROM netflix_titles
+
+
